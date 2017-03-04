4c85b7186f2e0df078de578972eee40c8040a7f7
Convezione <enrico.panetta@gmail.com>
2017-03-04 23:30:23 +0100

    RayCast Hit Implementation

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,8 @@
 #include "BuildingEscape.h"
 #include "Grabber.h"
 
+#define OUT
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -10,7 +12,10 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	
+	constexpr float PLAYER_REACH_FOR_GRABBING = 100.0f;
 
+	reach = PLAYER_REACH_FOR_GRABBING;
 	// ...
 }
 
@@ -19,7 +24,7 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
+	UE_LOG(LogTemp, Warning, TEXT("Grabber instance created!"));
 	// ...
 	
 }
@@ -30,6 +35,27 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player view point
+ 	APlayerController *player = GetWorld()->GetFirstPlayerController();
+	FRotator rotation;
+	FVector location;
+	player->GetPlayerViewPoint( OUT location, OUT rotation);
+	UE_LOG(LogTemp, Warning, TEXT("Parameters modified:\nLocation: %s\nRotation: %s"), *location.ToString(),*rotation.ToString());
+
+	//DrawDebugLine(GetWorld(), location, location + rotation.Vector()*reach, FColor(255, 0, 0, 255),false,0.0f,0.0f,10.0f);
+	
+	FHitResult objectHit;
+	FCollisionQueryParams parameters = FCollisionQueryParams(FName(TEXT("")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT objectHit,
+		location,
+		location + rotation.Vector() * reach,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		parameters
+		);
+	AActor* actorHit = objectHit.GetActor();
+	if (actorHit) {
+		UE_LOG(LogTemp,Warning,TEXT("Line trace hit: %s"), *(actorHit->GetName()))
+	}
 }
 
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -22,7 +22,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+private:
+	float reach;
 	
 };
